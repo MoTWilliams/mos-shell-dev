8e594b5096dcf32e398b28bc3f079b8b3a3581c2
Morgan Williams <morgachilla2018@gmail.com>
2025-04-24 01:59:16 -0500

    #defined a regex matching most file paths and a standard size for initializing buffers (32 elements)

---
+++ shell/utility/constants.h
@@ -0,0 +1,10 @@
+#ifndef CONSTANTS_H
+#define CONSTANTS_H
+
+// Matches most valid file paths
+#define PATH_REGEX "^[\\w@%+=:,./~\\-]+\\.(txt|sh)?$"
+
+// Initial size for automatically-resizing buffers
+#define BUFF_INIT 32
+
+#endif
