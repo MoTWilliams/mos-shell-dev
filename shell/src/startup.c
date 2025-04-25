2cf976b2160332889d1dcb4cab4447054af304ae
Morgan Williams <morgachilla2018@gmail.com>
2025-04-25 17:55:42 -0500

    Added if statement to open in appropriate mode based on startup command

--- shell/src/startup.c
+++ shell/src/startup.c
@@ -1,16 +1,35 @@
+#include "report_status.h"
+#include "frivolity.h"
+#include "ansi_codes.h"
+#include "shell.h"
+
 #include <stdio.h>
 #include <string.h>
 
 int main(int argc, char* argv[]) {
-        printf("\nWelcome to Mo's Shell!\n\n");
-        printf("DEBUG: %d args entered\n", argc);
-        
-        printf("  ARGS:\n");
-        for (int i = 0; argv[i]; i++) {
-                printf("  %d. %s\n", i, argv[i]);
+        // Startup command only has one optional argument
+        if (argc > 2) {
+                msg_input_error("too many arguments");
+                return 1;
         }
 
-        printf("\nGreat job! Goodbye!\n\n");
+        printf(FG_BLU_B BOLD "\nWelcome to Mo's Shell!\n\n" RESET);
+
+        if (argv[1]) {
+                printf(BG_RED "[--BATCH MODE UNDER CONSTRUCTION--]\n" RESET);
+        } else {
+                printf(FG_BLU_B "Entering interactive input mode" RESET);
+                friv_animatedEllipses(FG_BLU_B);
+                printf("\n");
+
+                runShell_interactive();
+
+                printf(FG_BLU_B "Exiting interactive input mode" RESET);
+                friv_animatedEllipses(FG_BLU_B);
+                printf("\n");
+        }
+
+        printf("Goodbye!\n\n");
 
         return 0;
 }
