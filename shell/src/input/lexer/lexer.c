3b8960545cb4633d643d00f1c0b704a2890b4177
Morgan Williams <morgachilla2018@gmail.com>
2025-05-21 00:15:13 -0500

    Lexer fully functional but could use some cleanup

--- shell/src/input/lexer/lexer.c
+++ shell/src/input/lexer/lexer.c
@@ -8,11 +8,14 @@
 #include <string.h>
 #include <ctype.h>
 
-void handle_escape(TokList* toks, char* input, int* pos, Bool* inTok);
-void skip_comment(TokList* toks, char* input, int* pos, Bool* inTok);
+#define IN_QUOTE(qContext)      ((qContext) == Q_SINGLE || \
+                                 (qContext) == Q_DOUBLE || \
+                                 (qContext) == Q_BACKTICK)
 
-void capture_symbol(TokList* toks, char* input, int* pos, Bool* inTok);
-void capture_words(TokList* toks, char* input, int* pos);
+void escape(TokList* toks,char* input,int* pos,Bool* inTok,QType qContext);
+void skip_comment(TokList* toks, char* input, int* pos, Bool* inTok);
+void capture_symbolTok(TokList* toks, char* input, int* pos, Bool* inTok);
+void capture_wordTok(TokList* toks, char* input, int* pos, Bool* inTok);
 
 TokList* input_lex(char* input) {
         /* Create an empty list to hold the tokens */
@@ -23,6 +26,7 @@ TokList* input_lex(char* input) {
 
         /* Split the input string into tokens */
         int pos = 0;
+
         while (1) {
                 /* Exit the loop when the null-terminator or EOF is reached */
                 if (!input[pos] || input[pos] == EOF) {
@@ -35,38 +39,29 @@ TokList* input_lex(char* input) {
                         continue;
                 }
 
-                /* Handle escape sequences outside of quoted strings: Current */
-                /* char is not the end of the string and is a backslash, and */
-                /* cursor is not inside a quoted string */
-                if (input[pos + 1] && input[pos] == '\\') {
-                        handle_escape(toks, input, &pos, &inTok);
-                        continue;
-                }
-
-                /* Skip comments: When a non-escaped # is detected, advance */
-                /* to  the end of the line without creating or adding to a */
-                /* token */
-                if (input[pos] == '#') {
-                        skip_comment(toks, input, &pos, &inTok);
-                        continue;
-                }
-
                 /* Capture operators (;,|,||,&,&&,<,<<,>,>>) and other */
                 /* symbols  (!,(),[],{}) */
-                if (strchr(";|&<>!()[]{}", input[pos])) {
-                        capture_symbol(toks, input, &pos, &inTok);
+                if (strchr(";|&<>!()", input[pos])) {
+                        capture_symbolTok(toks, input, &pos, &inTok);
                         continue;
                 }
 
-                
+                /* Everything else is a word */
+                capture_wordTok(toks, input, &pos, &inTok);
 
-                pos++;
+                /* Exit the loop and return the tokens if the line ends with an 
+                 * unterminated quoted string */
+                if (TOKS_TAIL(toks)->untermQ) {
+                        return toks;
+                }
+
+//                pos++;
         }
 
         return NULL;
 }
 
-void handle_escape(TokList* toks, char* input, int* pos, Bool* inTok) {
+void escape(TokList* toks,char* input,int* pos,Bool* inTok,QType qContext) {
         /* If not already in a token, create a new token */
         if (!(*inTok)) {
                 toks_addEmptyToken(toks);
@@ -74,39 +69,21 @@ void handle_escape(TokList* toks, char* input, int* pos, Bool* inTok) {
                 *inTok = TRUE;
         }
 
-        /* Special case: Escaping a newline discards both the backslash and */
-        /* the newline--will use in batch mode */
-        if (input[(*pos) + 1] == '\n') {
-                (*pos) += 2;
-                return;
-        }
+        /* Always gather the backslash and following char */
+        token_appendChar(TOKS_TAIL(toks), input, *pos, qContext);
+        token_appendChar(TOKS_TAIL(toks), input, (*pos) + 1, qContext);
 
-        /* Gather only the character following the backslash, not the */
-        /* backslash itself, and advance the cursor past both */
-        token_appendChar(TOKS_TAIL(toks), input, (*pos) + 1, Q_NONE);
-        (*pos) += 2;
-}
-
-void skip_comment(TokList* toks, char* input, int* pos, Bool* inTok) {
-        /* If cursor is inside a token, leave the token--handles if a # is */
-        /* detected without a word-terminating character preceding */
-        if (*inTok) {
+        /* Even escaped, a single quote while inside a single-quoted string 
+         * ends the token. */
+        if (qContext == Q_SINGLE && input[(*pos) + 1] == '\'') {
                 *inTok = FALSE;
         }
 
-        /* Skip the rest of the line */
-        while(1) {
-                /* Exit loop at the end of the line, string, or file */
-                if (!input[*pos] || input[*pos]=='\n' || input[*pos]==EOF) {
-                        return;
-                }
-
-                /* Advance without gathering anything */
-                (*pos)++;
-        }
+        // Advance down the input string accordingly
+        (*pos) += 2;
 }
 
-void capture_symbol(TokList* toks, char* input, int* pos, Bool* inTok) {
+void capture_symbolTok(TokList* toks, char* input, int* pos, Bool* inTok) {
         /* Always start a new token, if any of these chars are detected */
         /* outside of a quoted string */
         toks_addEmptyToken(toks);
@@ -114,12 +91,6 @@ void capture_symbol(TokList* toks, char* input, int* pos, Bool* inTok) {
 
         /* Add the current char to the token */
         token_appendChar(TOKS_TAIL(toks), input, *pos, Q_NONE);
-
-        /* These aren't special tokens. They're just captured as words with */
-        /* only one character */
-        if (strchr("[]{}", input[*pos])) {
-                TOKS_TAIL(toks)->tType = TOK_WORD;
-        }
 
         /* Build special symbol tokens */
         switch (input[*pos]) {
@@ -195,8 +166,135 @@ void capture_symbol(TokList* toks, char* input, int* pos, Bool* inTok) {
         *inTok = FALSE;
 }
 
-void capture_word(TokList* toks, char* input, int* pos) {
-        /* Just use the QType enum for control flow *and* metadata */
+/* Word token helpers */
+QType wordTok_getCharQType(char* input, int pos);
+
+void capture_wordTok(TokList* toks, char* input, int* pos, Bool* inTok) {
+        QType qContext = Q_NONE;         /* Cursor's quote context */
+        
+        /* If not already in a token, create a new token and move in to it */
+        if (!(*inTok)) {
+                toks_addEmptyToken(toks);
+                TOKS_TAIL(toks)->tType = TOK_WORD;
+                *inTok = TRUE;
+        }
+
+        /* These aren't special tokens. They're just captured as words with
+         * only one character */
+        if (strchr("[]{}", input[*pos])) {
+                TOKS_TAIL(toks)->tType = TOK_WORD;
+                token_appendChar(TOKS_TAIL(toks), input, *pos, Q_NONE);
+                (*pos)++;
+                *inTok = FALSE;
+                return;
+        }
+
+        /* Check for opening quote at first character */
+        QType firstCharQType = wordTok_getCharQType(input, *pos);
+        if (IN_QUOTE(firstCharQType)) {
+                qContext = firstCharQType;
+                TOKS_TAIL(toks)->untermQ = TRUE;
+        } else {
+                qContext = Q_NONE;
+        }
+        token_appendChar(TOKS_TAIL(toks), input, *pos, qContext);
+        (*pos)++;
+
+
+        /* Gather characters until a space or special character is reached */
+        while (1) {
+                QType curChrQType = Q_NONE;     /* Quote type of current char */
+                QType nextChrQType = Q_NONE;    /* Quote type of next char */
+
+                /* Exit the loop at end of input string or file */
+                if (!input[*pos] || input[*pos] == EOF) {
+                        break;
+                }
+
+                /* If a non-escaped # is reached outside quotes, treat as 
+                 * comment */
+                if (qContext == Q_NONE && input[*pos] == '#' &&
+                                (*pos == 0 || input[*pos - 1] != '\\')) {
+                        *inTok = FALSE;
+                        break;
+                }
+
+                /* Exit the loop when an operator or space is reached, if not 
+                 * in a quote */
+                if (qContext == Q_NONE && strchr(";|&<>! ", input[*pos])) {
+                        break;
+                }
+
+                /* Handle escape sequences inside of a word token */
+                if (input[(*pos) + 1] && input[*pos] == '\\') {
+                        escape(toks, input, pos, inTok, qContext);
+                }
+
+                /* If not in a quoted string and the current char is a quote 
+                 * mark, enter the appropriate quote context */
+                curChrQType = wordTok_getCharQType(input, *pos);
+                if (qContext == Q_NONE && IN_QUOTE(curChrQType)) {
+                        qContext = curChrQType;
+                        TOKS_TAIL(toks)->untermQ = TRUE;
+
+                        token_appendChar(
+                                TOKS_TAIL(toks), input, *pos, qContext
+                        );
+
+                        (*pos)++;
+                        continue;
+                }
+
+                /* Handle unterminated quotes and context changes while in 
+                 * quoted strings */
+                if (IN_QUOTE(qContext)) {
+                        /* Cursor reaches the end of the input line while 
+                         * inside a quoted string */
+                        if (curChrQType == qContext) {
+                                token_appendChar(
+                                        TOKS_TAIL(toks), input, *pos,qContext
+                                );
+                                qContext = Q_NONE;
+                                TOKS_TAIL(toks)->untermQ = FALSE;
+                                (*pos)++;
+                                continue;
+                        }
+
+                        /* Handle context changes while inside quotes: detect
+                         * closing quotes and change context to next char's 
+                         * qType */
+                        if (curChrQType == qContext) {
+                                token_appendChar(
+                                        TOKS_TAIL(toks),input,*pos,qContext
+                                );
+                                qContext = Q_NONE;
+                                TOKS_TAIL(toks)->untermQ = FALSE;
+                                (*pos)++;  // move past the closing quote
+                                continue;
+                        }
+                }
+
+                /* AND FINALLY: Add the next char to the current token */
+                token_appendChar(TOKS_TAIL(toks), input, *pos, qContext);
+                (*pos)++;
+        }
+
+        /* Move out of the token */
+        *inTok = FALSE;
+}
+
+QType wordTok_getCharQType(char* input, int pos) {
+        /* Set quote context */
+        switch (input[pos]) {
+                case '\'':
+                        return Q_SINGLE;
+                case '"':
+                        return Q_DOUBLE;
+                case '`':
+                        return Q_BACKTICK;
+                default: /* Not a quotation mark */
+                        return Q_NONE;
+        }
 }
 
 /* TokList methods */
@@ -247,12 +345,27 @@ void toks_print(TokList* toks) {
         /* Print the list of tokens */
         printf("Tokens: [");
         while (current) {
-                printf("\"");
                 token_print(current->data.token);
-                printf("\"");
                 if (current->next) {
                         printf(", ");
                 }
+                current = current->next;
+        }
+        printf("]\n");
+
+        current = toks->tokList->head;
+        printf("Quote Contexts: [");
+        while (current) {
+                char* types = STR_TEXT(current->data.token->cqTypes);
+
+                printf("%s", types);
+
+                if (current->next) {
+                        printf(", ");
+                }
+
+                free(types);
+
                 current = current->next;
         }
         printf("]\n");
