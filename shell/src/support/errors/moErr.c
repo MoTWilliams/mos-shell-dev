7538f1ee79b51f466401c31dbe8aa4c63929da9b
Morgan Williams <morgachilla2018@gmail.com>
2025-05-30 19:38:20 -0500

    changed snprintf() to sprintf() for C89 compliance

--- shell/src/support/errors/moErr.c
+++ shell/src/support/errors/moErr.c
@@ -3,34 +3,31 @@
 #include "textStylesAndColors.h"
 
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 
-#define REGISTER_ERR(errNo) \
-        len += snprintf( \
-                splash + len, sizeof(splash) - len, "%s", #errNo + 4 \
-        )
+#define REGISTER_ERR(errNo) sprintf(splash + len, "%s", #errNo + 4)
 
 ErrNo report_err(Fatality fatal, ErrNo errNo, char* msg,
-                        const char* file, int line, const char* func) {
+                        const char* file, int line) {
         /* The error type as a string--max 1028 characters */
         char splash[1028 + 1] = {'\0'};
-        
-        /* If the error is fatal, add FATAL- to the error splash */
-        size_t len = snprintf(
-                splash, sizeof(splash), "%s", fatal ? "FATAL_" : ""
-        );
+
+        size_t len = 0;
+        sprintf(splash, "%s", fatal ? "FATAL_" : "");
+        len = strlen(splash); 
 
         /* This function was called in error or with ERR_NONE (0) */
         if (!errNo) {
                 printf(FG_GRN "Not actually an error\n" RESET);
         }
-
+        
         /* Add the error code to the [ERROR_SPLASH]*/
         REGISTER_ERR(errNo);
 
         /* Display the error splash and message */
         fprintf(stderr, (FG_RED "[" BOLD "%s" UNBOLD "] " RESET), splash);
-        fprintf(stderr, "FILE: \"%s\"; LINE: %d; FUNCTION: \"%s\"; %s\n",
-                                                        file, line, func, msg);
+        fprintf(stderr, "FILE: \"%s\"; LINE: %d; %s\n", file, line, msg);
         
         /* Exit the program immediately on fatal error */
         if (fatal) {
